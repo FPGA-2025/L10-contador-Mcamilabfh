dadc9e874ad3c45d7ca2421dac23685574e99202
Maria Camila <mcamilabfh@gmail.com>
2025-04-26 01:41:40 -0300

    Tarefa 10 Contador

--- contador.v
+++ contador.v
@@ -1,13 +1,22 @@
 module Contador (
-    input  wire clk,
-    input  wire rst_n,
-
-    input  wire acrescer,
-    input  wire decrecer,
-
+    input wire clk,
+    input wire rst_n,
+    input wire acrescer,
+    input wire decrecer,  // <- aqui tá o detalhe!
     output reg [7:0] saida
 );
 
-// Insira seu código aqui
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        saida <= 8'd106;
+    end else begin
+        if (acrescer && !decrecer) begin
+            saida <= saida + 1;
+        end else if (!acrescer && decrecer) begin
+            saida <= saida - 1;
+        end
+    end
+end
 
 endmodule
